34ed1e4f07f7551c4dd7583483526c6f97efd3cd
Kingsley Chan <chan.kingsley@gmail.com>
2018-03-17 12:57:19 +0800

    add routes model class

---
+++ myapp/application/models/Routes_model.php
@@ -0,0 +1,51 @@
+<?php
+
+class Routes_model extends CI_Model {
+	public function __construct(){
+		if(!$this->db->table_exists('routes')){
+			$this->load->library('migration');
+
+			if ($this->migration->current() === FALSE){
+				show_error($this->migration->error_string());
+	        }
+		}
+	}
+
+	public function insertRoute($data = []){
+		return $this->db->where('token', $data['token'])
+			->replace('routes', $data);
+	}
+
+	public function tokenExists($token = false){
+		if($token){
+			$result = $this->getByToken($token);
+
+			if($result){
+				return [
+					'exists' => true,
+					'error' => $result['error']
+				];
+			}
+
+			return [
+				'exists' => false,
+				'error' => 'TOKEN_NOT_EXISTS'
+			];
+		}
+
+		return [
+			'exists' => false,
+			'error' => 'EMPTY_TOKEN'
+		];
+	}
+
+	public function getByToken($token = false){
+		if($token){
+			return $this->db->get_where('routes', [
+				'token' => $token
+			])->row_array();
+		}
+
+		return ;
+	}
+}
